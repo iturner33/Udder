b00d5bbf22ccba6b2f3819fc6a92c998026d59f1
Ian Turner <ian.turner@5igm.co.uk>
2020-03-15 16:12:56 +0000

    First Commit of the Udder App

---
+++ Udder/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Udder
+//
+//  Created by Ian Turner on 15/03/2020.
+//  Copyright © 2020 Ian Turner. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Udderly Fabulous")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
