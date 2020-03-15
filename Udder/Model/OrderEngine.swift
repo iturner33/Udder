6dfef801052b77817b41c1a503bd5f829a0ea149
Ian Turner <ian.turner@5igm.co.uk>
2020-03-15 16:18:02 +0000

    Second Commit added Model

---
+++ Udder/Model/OrderEngine.swift
@@ -0,0 +1,14 @@
+//
+//  OrderEngine.swift
+//  Udder
+//
+//  Created by Ian Turner on 15/03/2020.
+//  Copyright © 2020 Ian Turner. All rights reserved.
+//
+
+import Foundation
+
+struct Order {
+  let product: String
+  let quanity: Int
+}
